b39c232359bdf98bee59b0a1c8e3992364fc021a
shambu <shambuqwerty@gmail.com>
2021-11-07 21:00:01 +0530

    Minor Update

--- deploy.js
+++ deploy.js
@@ -13,10 +13,11 @@ const web3 = new Web3(provider);
 const deploy = async () => {
 	const accounts = await web3.eth.getAccounts();
 	console.log(`Attempting to deploy from account ${accounts[0]}`);
-	const ressult = await new web3.eth.Contract(JSON.parse(interface))
+	const result = await new web3.eth.Contract(JSON.parse(interface))
 		.deploy({ data: bytecode })
 		.send({ gas: "1000000", from: accounts[0] });
 
-	console.log(`Contract deployed to ${ressult.options.address}`);
+	console.log(interface);
+	console.log(`Contract deployed to ${result.options.address}`);
 };
 deploy();
